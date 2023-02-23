6bc2f72a8e1ae75bb67f44dc6dea7c48231481a9
Azzam Mahmoud <azzammahmoud007@gmail.com>
2023-02-23 19:22:44 +0200

    unfinished currencies models and controllers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\currencyController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/currency',[currencyController::class,'getAllCurrency']);
+Route::get('/currency/{id}',[currencyController::class,'getCurrency']);
+Route::post('/currency',[currencyController::class,'addCurrency']);
+Route::Patch('/currency/{id}',[currencyController::class,'editCurrency']);
+Route::delete('/currency/{id}',[currencyController::class,'deleteCurrency']);
+
