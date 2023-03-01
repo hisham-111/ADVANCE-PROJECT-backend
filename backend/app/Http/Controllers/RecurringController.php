957cb6ade2e590ebbb05b403b830d52324aecd85
fatima-katrib <fatimakatrib19@gmail.com>
2023-03-01 16:43:08 +0200

    add currency relationship

--- backend/app/Http/Controllers/RecurringController.php
+++ backend/app/Http/Controllers/RecurringController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Recurring;
+use App\Models\Currency;
 use Illuminate\Support\Facades\Validator;
 
 class RecurringController extends Controller
@@ -40,20 +41,24 @@ class RecurringController extends Controller
         $amount=$request->input('amount');
         $start_date=$request->input('start_date');
         $end_date=$request->input('end_date');
+        $currency_id=$request->input('currency_id');
+        $currency = Currency::find($currency_id);
+        // $category_id=$request->input('category_id');
       
         $validator = Validator::make($request->all(), [
-            'title' => 'required|string',
-            'description' => 'required|string',
+            'title' => 'required|string|max:100',
+            'description' => 'required|string|max:250',
             'amount' => 'required|numeric|gt:0',
-            // 'currency_id'=> 'required|integer|min:1' ****************************
-            // 'category_id' => 'required|integer|min:1', ****************************
-            "start_date" => 'required|date_format:Y-m-d|before_or_equal:' . date('Y-m-d'),
-            "end_date" => 'required|date_format:Y-m-d|after:start_date'
+            'currency_id'=> 'required|integer|min:1|exists:currencies,id',
+            // 'category_id' => 'required|integer|min:1|exists:categories,id',
+            "start_date" => 'required|date_format:Y-m-d|before_or_equal:today',
+            "end_date" => 'required|date_format:Y-m-d|after:start_date',
         ]);
         $recurring->title = $title;
         $recurring->description = $description;
         $recurring->amount = $amount;
-        // $recurring->currency_id=$currency_id;
+        $recurring->currency()->associate($currency);;
+
         // $recurring->category_id=$category_id;
         $recurring->start_date = $start_date;
         $recurring->end_date = $end_date;
@@ -67,7 +72,7 @@ class RecurringController extends Controller
     public function edit(Request $request, $id){
         $recurring =  Recurring::findOrFail($id);
         $inputs= $request
-        ->except('_method')
+        ->except('category','currency','_method')
         ;
         $recurring->update($inputs);
         // if($request->has('category')){
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -20,6 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/currency',[currencyController::class,'getAllCurrency']);
+Route::get('/currency/{id}',[currencyController::class,'getCurrency']);
+Route::post('/currency',[currencyController::class,'addCurrency']);
+Route::patch('/currency/{id}',[currencyController::class,'editCurrency']);
+Route::delete('/currency/{id}',[currencyController::class,'deleteCurrency']);
 
 Route::get('/recurrings',[RecurringController::class,'index']);
 Route::get('/recurrings/{id}',[RecurringController::class,'show']);
